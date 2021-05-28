bdac38b811575507c96bb65d0ce639d270f2a8b8
nandozanutto <nando.zanutto@gmail.com>
2021-05-28 10:41:23 -0300

    initial files

---
+++ SistemasLineares.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include "utils.h"
+#include "SistemasLineares.h"
+
+/*!
+  \brief Essa função calcula a norma L2 do resíduo de um sistema linear 
+
+  \param SL Ponteiro para o sistema linear
+  \param x Solução do sistema linear
+  \param res Valor do resíduo
+
+  \return Norma L2 do resíduo.
+*/
+real_t normaL2Residuo(SistLinear_t *SL, real_t *x, real_t *res)
+{
+
+}
+
+
+/*!
+  \brief Método da Eliminação de Gauss
+
+  \param SL Ponteiro para o sistema linear
+  \param x ponteiro para o vetor solução
+  \param tTriangulariza tempo gasto na triangularização
+  \param tRetroSubst tempo gasto na retrosubstituição
+
+  \return código de erro. 0 em caso de sucesso.
+*/
+int eliminacaoGauss (SistLinear_t *SL, real_t *x, double *tTotal)
+{
+
+
+}
+
+/*!
+  \brief Método de Jacobi
+
+  \param SL Ponteiro para o sistema linear
+  \param x ponteiro para o vetor solução. Ao iniciar função contém
+            valor inicial
+  \param erro menor erro aproximado para encerrar as iterações
+  \param tTotal tempo gasto pelo método
+  \param tIteração tempo gasto em cada iteração
+
+  \return código de erro. Um nr positivo indica sucesso e o nr
+          de iterações realizadas. Um nr. negativo indica um erro:
+          -1 (não converge) -2 (sem solução)
+*/
+int gaussJacobi (SistLinear_t *SL, real_t *x, double *tTotal)
+{
+
+
+}
+
+/*!
+  \brief Método de Gauss-Seidel
+
+  \param SL Ponteiro para o sistema linear
+  \param x ponteiro para o vetor solução. Ao iniciar função contém
+            valor inicial
+  \param erro menor erro aproximado para encerrar as iterações
+  \param tTotal tempo gasto pelo método
+  \param tIteração tempo gasto em cada iteração
+
+  \return código de erro. Um nr positivo indica sucesso e o nr
+          de iterações realizadas. Um nr. negativo indica um erro:
+          -1 (não converge) -2 (sem solução)
+  */
+int gaussSeidel (SistLinear_t *SL, real_t *x, double *tTotal)
+{
+
+
+}
+
+
+/*!
+  \brief Método de Refinamento
+
+  \param SL Ponteiro para o sistema linear
+  \param x ponteiro para o vetor solução. Ao iniciar função contém
+            valor inicial para início do refinamento
+  \param erro menor erro aproximado para encerrar as iterações
+
+  \return código de erro. Um nr positivo indica sucesso e o nr
+          de iterações realizadas. Um nr. negativo indica um erro:
+          -1 (não converge) -2 (sem solução)
+  */
+int refinamento (SistLinear_t *SL, real_t *x, double *tTotal)
+{
+
+
+}
+
+/*!
+  \brief Alocaçao de memória 
+
+  \param n tamanho do SL
+
+  \return ponteiro para SL. NULL se houve erro de alocação
+  */
+SistLinear_t* alocaSistLinear (unsigned int n)
+{
+
+
+}
+
+/*!
+  \brief Liberaçao de memória 
+
+  \param sistema linear SL
+  */
+void liberaSistLinear (SistLinear_t *SL)
+{
+
+
+}
+
+/*!
+  \brief Leitura de SL a partir de Entrada padrão (stdin).
+
+  \return sistema linear SL. NULL se houve erro (leitura ou alocação)
+  */
+SistLinear_t *lerSistLinear ()
+{
+  
+}
+
+
+// Exibe SL na saída padrão
+void prnSistLinear (SistLinear_t *SL)
+{
+
+}
+
+// Exibe um vetor na saída padrão
+void prnVetor (real_t *v, unsigned int n)
+{
+
+}
+
---
+++ labSisLin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "utils.h"
+#include "SistemasLineares.h"
+
+int main ()
+{
+
+
+}
+
